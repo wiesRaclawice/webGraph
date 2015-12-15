9fc5688ca30076af6d9b49ce9b87cd155fb5a7bf
Sabina Rydzek <sabina.rydzek@gmail.com>
2015-12-15 20:28:01 +0100

    changes not working

--- js/scripts/mainSVG.js
+++ js/scripts/mainSVG.js
@@ -1,3 +1,30 @@
+var WebGraph = {};
+
+//PUBLIC INTERFACE
+WebGraph = {
+    //engine
+    initGraph : function() { return false; },
+    clearGraph : function() { return false;},
+    loadGraph : function() { return false;},
+    saveGraph : function() {return false;},
+    //graph
+    createNode : function(x, y) {return WebGraph.Engine.createNode(x, y);},
+    deleteNode : function() {return false;},
+    createEdge : function() {return false;},
+    deleteEdge : function() {return false;}
+};
+
+WebGraph.Engine = {
+    createNode : function(x, y) {
+        $data =  '<svg height="100" width="100">' +
+            '<circle cx="' + x + '" cy="' + y + '" r="40" stroke="black" stroke-width="3" fill="red" />' +
+            'Sorry, your browser does not support inline SVG.' +
+            '</svg>';
+
+        $('#mainBoard').append($data);
+    }
+};
+
 function load() {
 
     $contextMenu = null;
@@ -18,7 +45,7 @@ function load() {
         $('body').append($contextMenu);
         $('#context-menu-new-node').click(function (e) {
             e.preventDefault();
-            alert("New node!");
+            WebGraph.createNode(ev.pageX, ev.pageY);
             $contextMenu.remove();
         });
         $('#mainBoard').click(function (e) {
